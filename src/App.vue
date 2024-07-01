eca7236ddfaec61add5b2a611a7a9294e97d69c5
Abrham <abrsh6266@gmail.com>
2024-07-01 10:29:19 -0800

    adding navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+  <v-app id="home">
+    <Navbar />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from '@/components/Navbar.vue'
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
+    Navbar
   },
 
   data: () => ({
